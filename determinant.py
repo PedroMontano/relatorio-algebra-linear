8f0849cb71c8efb667b0f9cbf7dcdb549d78a482
vinizm <vmartins@poli.ufrj.br>
2021-05-19 20:19:57 -0300

    script to calculate determinants

---
+++ determinant.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def laplace( A: np.ndarray ):
+    # choose element
+    # choose direction
+    # get new matrix
+    # calculate determinant by Laplace formula
+    # return determinant
